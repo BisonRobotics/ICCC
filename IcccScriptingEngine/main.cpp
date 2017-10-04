0f8b7b4cc310ab194620a371aa5fbbf59953c07b
Joey Cluett <joesunglassescool@gmail.com>
2017-10-04 16:17:54 -0500

    first commit of Scripting language project

---
+++ IcccScriptingEngine/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "SinglyLinkedList.h"
+#include "IcccTokenizer.h"
+
+using namespace std;
+
+/** main function **/
+int main(int argc, char* argv[]) {
+    TokenStream ts("sample.iccc");
+
+
+
+    return 0;
+}
