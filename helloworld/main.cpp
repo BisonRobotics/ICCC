6592992d3bc92b9c7e7b0271a22012f0d19dd3ca
JoeyCluett <joesunglassescool@gmail.com>
2019-10-03 18:35:58 -0500

    added helloworld program

---
+++ helloworld/main.cpp
@@ -0,0 +1,19 @@
+// import the input/output library
+#include <iostream>
+
+// function declaration
+// returns 'int', called 'main', takes 'void' argument
+int main(void) 
+{ // function definition
+
+    // use item from input/output library (std::cout)
+    // to print data to screen
+    std::cout 
+        << "Hello World" // print raw text string
+        << ' '           // print single character 
+        << 12345         // print integer
+        << std::endl;    // print newline
+
+    return 0;
+
+}
